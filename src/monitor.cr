7973da825f3604fc5c6c399d56cfd4408b996503
Bar Hofesh <bar.hofesh@gmail.com>
2022-11-23 19:57:19 +0200

    more helpers

--- src/monitor.cr
+++ src/monitor.cr
@@ -30,27 +30,23 @@ module Monitor
       usage "monitor --url <url>"
       version "Version 0.1.0"
       option "-u URL", "--url=URL", type: String, desc: "Target URL.", required: true
+      option "-c CONCURRENCY", "--concurrency=CONCURRENCY", type: Int32, desc: "Number of concurrent requests to make.", default: 10
+      option "-t TOTAL_REQUESTS", "--total-requests=TOTAL_REQUESTS", type: Int32, desc: "Total number of requests to make.", default: 1000
       run do |opts, args|
         uri = URI.parse(opts.url)
         unless uri.host && uri.scheme
           raise "Error: URL Is Malformed"
         end
-        requests_number = 1_000
         response_channel = Channel(Int32 | Exception).new
+        uri_channel = Channel(URI).new
+        opts.concurrency.times do
+          spawn do
+            request_handlers(uri_channel, response_channel)
+          end
+        end
         spawn do
-          requests_number.times do
-            spawn do
-              client = HTTP::Client.new(uri)
-              client.read_timeout = 30.seconds
-              client.connect_timeout = 30.seconds
-              response = client.get(uri.path || "/")
-
-              response_channel.send(response.status_code)
-            rescue e : Exception
-              response_channel.send(e)
-            ensure
-              client.close if client
-            end
+          opts.total_requests.times do
+            uri_channel.send(uri)
           end
         end
 
@@ -61,7 +57,7 @@ module Monitor
           wafs = Wafalyzer.detect(url: uri.to_s)
         rescue Exception
         end
-        requests_number.times do |i|
+        opts.total_requests.times do |i|
           response = response_channel.receive
           if response.is_a?(Exception)
             unless exception_messages[response]?
@@ -91,6 +87,22 @@ module Monitor
           system("clear")
           puts "WAFs detected: #{wafs.map(&.to_s).join(", ")}".colorize(:red).mode(:bold) unless wafs.empty?
           puts table
+        end
+      end
+
+      def request_handlers(uri_channel : Channel(URI), response_channel : Channel(Int32 | Exception))
+        loop do
+          uri = uri_channel.receive
+          client = HTTP::Client.new(uri)
+          client.read_timeout = 30.seconds
+          client.connect_timeout = 30.seconds
+          response = client.get(uri.path || "/")
+
+          response_channel.send(response.status_code)
+        rescue e : Exception
+          response_channel.send(e)
+        ensure
+          client.close if client
         end
       end
     end
